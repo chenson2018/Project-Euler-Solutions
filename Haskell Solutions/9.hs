38379e64d1884bc3d389df17ad59f68b4b6e890d
chenson2018 <46805207+chenson2018@users.noreply.github.com>
2020-04-17 06:38:33 -0500

    Comments

--- Haskell Solutions/9.hs
+++ Haskell Solutions/9.hs
@@ -8,8 +8,11 @@ takeWhileInclusive :: (a -> Bool) -> [a] -> [a]
 takeWhileInclusive _ [] = []
 takeWhileInclusive p (x:xs) = x : if p x then takeWhileInclusive p xs
                                          else []
-
+                                         
+--these functions allow us to generate all primitive triples
+--i.e. ones where a, b, c are coprime
 --https://stackoverflow.com/a/7261896/11090784
+
 merge f x [] = x
 merge f [] y = y
 merge f (x:xs) (y:ys)
@@ -35,8 +38,14 @@ triplets = map (\[[a],[b],[c]] -> [a, b, c]) $ x : triplegen_helper x  where x =
 
 main =  do
     let n = 1000
+        --get primitives with perimeter less than or equal 1000    
         primitives = takeWhile ((<=n) . sum) triplets
+        
+        --helper function to get non-primitive multiples
         multiples n = (map.map) (*n) primitives
-        test = filter ((==n) . sum) $ concat $ map (multiples) [1..100]
+        
+        --the smallest triple is (3, 4, 5) with sum 12
+        --this means checking up to multiples of 84 will be sufficient
+        test = filter ((==n) . sum) $ concat $ map (multiples) [1..84]
         
     print test
