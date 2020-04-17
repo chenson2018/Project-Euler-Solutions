2de4d20ab096058261ccb233def9f56811b805d0
chenson2018 <46805207+chenson2018@users.noreply.github.com>
2020-04-17 08:57:14 -0500

    Rewrote Pythagorean Tree

--- Haskell Solutions/9.hs
+++ Haskell Solutions/9.hs
@@ -1,51 +1,49 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-13.7 script
 
-import Data.List 
-import Data.Maybe
+import Data.Matrix
+import Data.List
+import Data.Ord
 
-takeWhileInclusive :: (a -> Bool) -> [a] -> [a]
-takeWhileInclusive _ [] = []
-takeWhileInclusive p (x:xs) = x : if p x then takeWhileInclusive p xs
-                                         else []
-                                         
---these functions allow us to generate all primitive triples
---i.e. ones where a, b, c are coprime
---https://stackoverflow.com/a/7261896/11090784
+--see https://en.wikipedia.org/wiki/Tree_of_primitive_Pythagorean_triples
 
-merge f x [] = x
-merge f [] y = y
-merge f (x:xs) (y:ys)
-               | f x y     =  x : merge f xs     (y:ys) 
-               | otherwise =  y : merge f (x:xs) ys 
+seed :: Matrix Int
+seed = fromList 3 1 [3, 4, 5]
+
+a :: Matrix Int
+a = fromLists [[ 1, -2, 2],
+               [ 2, -1, 2],
+               [ 2, -2, 3]]
+
+b :: Matrix Int
+b = fromLists [[ 1, 2, 2],
+               [ 2, 1, 2],
+               [ 2, 2, 3]]
+ 
+c :: Matrix Int 
+c = fromLists [[-1, 2, 2],
+               [-2, 1, 2],
+               [-2, 2, 3]]
+
+next_branch :: [Matrix Int] -> [Matrix Int]
+next_branch triplets = concat $ map (\xs -> [a*xs, b*xs, c*xs]) triplets
 
 
-mmult :: Num a => [[a]] -> [[a]] -> [[a]] 
-mmult a b = [ [ sum $ zipWith (*) ar bc | bc <- (transpose b) ] | ar <- a ]
 
-tpgen_matrix = [[[ 1,-2, 2],[ 2 ,-1, 2],[ 2,-2, 3]],
-                [[ 1, 2, 2],[ 2 , 1, 2],[ 2, 2, 3]],
-                [[-1, 2, 2],[-2 , 1, 2],[-2, 2, 3]]]
-
-matrixsum  =  sum . map sum
-tripletsorter x y =  ( matrixsum  x ) < ( matrixsum y ) -- compare perimeter
-
-triplegen_helper b =  foldl1 
-            ( merge tripletsorter ) 
-            [ h : triplegen_helper h | x <- tpgen_matrix , let h = mmult x b ]
-
-triplets = map (\[[a],[b],[c]] -> [a, b, c]) $ x : triplegen_helper x  where x = [[3],[4],[5]]
-
-main =  do
+main = do
     let n = 1000
-        --get primitives with perimeter less than or equal 1000    
-        primitives = takeWhile ((<=n) . sum) triplets
         
+        --the tree grows quickly, so I just take the first five layers
+        primitives = filter ( (<n) . sum) $
+                     concat $
+                     take 5 $
+                     ((map.map) toList $ iterate next_branch [seed])
+                     
         --helper function to get non-primitive multiples
         multiples n = (map.map) (*n) primitives
         
         --the smallest triple is (3, 4, 5) with sum 12
         --this means checking up to multiples of 84 will be sufficient
-        test = filter ((==n) . sum) $ concat $ map (multiples) [1..84]
-        
-    print test
+        ans = filter ((==n) . sum) $ concat $ map (multiples) [1..84]        
+    
+    print ans
